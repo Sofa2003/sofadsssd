854d24a205c74cafc487cbcbe05aac862f9bacb5
Sofa2003 <sborovkova168@gmail.com>
2023-12-26 13:39:42 +0700

    First Commit

---
+++ app/src/main/java/com/example/dssss/SecondActivity.java
@@ -0,0 +1,31 @@
+package com.example.dssss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_second);
+        TextView textView= new TextView(this);
+        textView.setTextSize(26);
+        textView.setPadding(16, 16,16,16);
+
+
+
+        Bundle arguments=getIntent().getExtras();
+
+        User user;
+        if(arguments!=null){
+            user = (User) arguments.getSerializable(User.class.getSimpleName());
+
+            textView.setText("Name: " + user.getName() + "\nCompany: " + user.getCompany() +
+                    "\nAge: " + String.valueOf(user.getAge()));
+        }
+        setContentView(textView);
+    }
+}
